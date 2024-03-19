f6caec285f3269566a67eb315a8967a6b75e34e4
Chorlte my balls <philmattdev@gmail.com>
2024-03-19 22:27:19 +0800

    feat: RegisterUserResponse schema

--- fastauth/models/schemas.py
+++ fastauth/models/schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
+from fastauth.models.user import FastUser
+
 
 class RegisterUserRequest(BaseModel):
     email: str
     password: str
+
+
+class RegisterUserResponse(BaseModel):
+    user: FastUser
