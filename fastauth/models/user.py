12ae0da99b57c5f1b3c851a10c16478527b57c6e
Chorlte my balls <philmattdev@gmail.com>
2024-03-19 22:17:56 +0800

    feat: access_token field in FastUser document

--- fastauth/models/user.py
+++ fastauth/models/user.py
@@ -6,8 +6,10 @@ from pydantic import BaseModel, Field
 
 class FastUser(Document):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+
     email: str
     password: bytes
+    access_token: str
 
     class Settings:
         name = "users"
