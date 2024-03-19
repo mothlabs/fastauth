5d3d7a0ac8cc64a31e760e25a8d40b8da4b289e4
Chorlte my balls <philmattdev@gmail.com>
2024-03-19 22:28:17 +0800

    feat: register endpoint

--- fastauth/fastauth.py
+++ fastauth/fastauth.py
@@ -2,6 +2,7 @@ from typing import Type
 
 from fastapi import APIRouter, FastAPI
 
+from fastauth.models.schemas import RegisterUserRequest, RegisterUserResponse
 from fastauth.models.user import FastUser
 from fastauth.services import AuthenticationService
 
@@ -19,3 +20,14 @@ class FastAuth:
 
     def register(self, app: FastAPI) -> None:
         app.include_router(self.router)
+
+    def _register_routers(self) -> None:
+
+        @self.router.post("/register")
+        async def register(body: RegisterUserRequest) -> RegisterUserResponse:
+
+            user = self.service.register_user(
+                **body.model_dump_json(),
+            )
+
+            return RegisterUserResponse(user=user)
