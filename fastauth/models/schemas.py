cadc0198d4787d85149b21899de29c110461e874
Chorlte my balls <philmattdev@gmail.com>
2024-03-22 20:26:01 +0800

    feat: add schemas for login endpoint

--- fastauth/models/schemas.py
+++ fastauth/models/schemas.py
@@ -10,3 +10,13 @@ class RegisterUserRequest(BaseModel):
 
 class RegisterUserResponse(BaseModel):
     user: FastUser
+
+
+class LoginUserRequest(BaseModel):
+    email: str
+    password: str
+
+
+class LoginUserResponse(BaseModel):
+    user: FastUser
+    access_token: str
